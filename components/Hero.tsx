203ef181b9543f20430efce46d5fa37cf7ef07b4
Naaleeyeportal <naaleeyeportal@gmail.com>
2024-07-02 16:58:30 +0300

    SEO updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='flex flex-col items-center justify-center bg-gray-100 py-8 sm:py-16 px-4'>
       <div className='max-w-4xl text-center'>
         <h1 className='text-2xl sm:text-4xl text-left font-bold mb-4'>
-          Madal aqris <span className='text-[#f63d68]'>Maqalka</span> waa madal lagu xaqiijiyo sax ahaanshaha xogaha dhagaheena soo gaaro ee inooga yimaada bulshooyinka.
+          Madal aqris; <span className='text-[#f63d68]'>Maqalka</span> waa madal lagu xaqiijiyo sax ahaanshaha xogaha dhagaheena soo gaaro ee inooga yimaada bulshooyinka.
 
         </h1>
         <p className='text-base sm:text-lg text-gray-700 leading-relaxed text-left'>
