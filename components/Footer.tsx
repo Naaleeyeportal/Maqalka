16443bb132c2a15c6958fbfd4fc3022ae24e8343
Naaleeyeportal <naaleeyeportal@gmail.com>
2024-07-29 20:24:49 +0300

    Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,16 +12,16 @@ const Footer = () => {
       <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0'>
           <div className='flex space-x-4'>
-            <Link href='/' className='text-gray-100 hover:text-red-400' target='_blank' >
+            <Link href='/' className='text-gray-100 hover:text-red-400'  >
               Home
             </Link>
-            <Link href='/about' className='text-gray-100 hover:text-red-400' target='_blank' >
+            <Link href='/about' className='text-gray-100 hover:text-red-400'  >
               About
             </Link>
-            <Link href='/contact' className='text-gray-100 hover:text-red-400' target='_blank' >
+            <Link href='/contact' className='text-gray-100 hover:text-red-400'  >
               Contact
             </Link>
-            <Link href='/blogs' className='text-gray-100 hover:text-red-400' target='_blank' >
+            <Link href='/blogs' className='text-gray-100 hover:text-red-400'  >
               Blogs
             </Link>
           </div>
